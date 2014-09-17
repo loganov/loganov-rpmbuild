8886f568da3b13bac02a620b1bac349e80c231db
Gregory Weaver <gregory.weaver@gmail.com>
2014-09-17 16:19:56 -0700

    Dependencies.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "loganov-java"
-include_recipe "loganov-nginx"
 include_recipe "loganov-jenkins"
+include_recipe "loganov-nginx"
 
 yumgroup "Development Tools" do
 	action :install 
